292015ced7352784ccf7386aa5fe92a61809611c
adam <y13922847770@163.com>
2024-11-07 16:00:23 +0800

    abcd

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { tours } from '../data'
 
 function Tours() {
   return (
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from '../images/Strike-freedom-v2.png';
+import tour2 from '../images/Infinite-Justice-Gundam-v2.png';
+import tour3 from '../images/Destiny-Gundam-v2.png';
+import tour4 from '../images/Legend-Gundam-v2.png';
+
 export const pageLinks =[
     {id: 1, href: '#home', text: 'home'},
     {id: 2, href: '#about', text: 'about'},
@@ -11,3 +16,10 @@ export const iconLinks =[
     {id: 3, href: "https://store.steampowered.com/app/1672500/GUNDAM_4/",icon: "fa-brands fa-steam"},
     {id: 4, href: "https://www.instagram.com/gundaminfona/reel/C_Q1HEmRPnc/",icon: "fa-brands fa-instagram"},
 ]
+
+export const tours =[
+    {id : 1, image: 'tour1', date: 'Aug 26th, 2024', title: 'Strike Freedom', info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, possimus?', location: 'robot', duration: '6', cost: 'from $2100'},
+    {id : 2, image: 'tour2', date: 'Aug 26th, 2024', title: 'Infinite Justice Gundam', info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, possimus?', location: 'robot', duration: '6', cost: 'from $2100'},
+    {id : 3, image: 'tour3', date: 'Aug 26th, 2024', title: 'Destiny Gundam', info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, possimus?', location: 'robot', duration: '6', cost: 'from $2100'},
+    {id : 4, image: 'tour4', date: 'Aug 26th, 2024', title: 'Legend Gundam', info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, possimus?', location: 'robot', duration: '6', cost: 'from $2100'},
+] 
